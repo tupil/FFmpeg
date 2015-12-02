157c691c179a044c260a248ec2ff466fc84fb333
Eelco Lempsink <eml@tupil.com>
2015-12-02 16:54:31 +0100

    libavcodec/ass_split: Handle conversion of ASS/SSA subtitles without Events section
    According to the SSA spec, the Events section does not have to be part of the
    extradata/subtitle_header. Conversion to other formats (srt/webvtt) failed in
    this case.

--- libavcodec/ass_split.c
+++ libavcodec/ass_split.c
@@ -412,10 +412,17 @@ ASSDialog *ff_ass_split_dialog(ASSSplitContext *ctx, const char *buf,
                                int cache, int *number)
 {
     ASSDialog *dialog = NULL;
-    int i, count;
+    int i, j, count;
     if (!cache)
         for (i=0; i<FF_ARRAY_ELEMS(ass_sections); i++)
             if (!strcmp(ass_sections[i].section, "Events")) {
+                /* If the subtitle_header did not contain an ‘Events’ section, initialize as default */
+                if (ctx->current_section != i) {
+                    ctx->current_section = i;
+                    ctx->field_order[i] = get_default_field_orders(&ass_sections[i], number);
+                    for (j=0; ctx->field_order[i][j] != -1; j++);
+                    ctx->field_number[i] = j;
+                }
                 free_section(ctx, &ass_sections[i]);
                 break;
             }
